6134a99b0389b52ce6cf7e36ea321040b51461be
kstagaman <kstagaman@gmail.com>
2022-07-19 20:35:08 -0700

    changed nds() to %d%

--- R/nds.R
+++ R/nds.R
@@ -1,16 +1,15 @@
-#' @name nds
+#' @name "%d%"
 #' @title roll n dice of side s
 #' @description Roll some dice!
-#' @param n integer; number of dice to roll.
-#' @param s integer; number of sided die to roll.
-#' @param sum logical; if TRUE, return the sum of the results, if false, return a vector of results equal in length to `n`. Default is TRUE.
+#' @param n integer; number of dice to roll
+#' @param s integer; number of sided die to roll
 #' @seealso \code{\link{sample}}
 #' @export
 #' @examples
 #' # Roll 4d6!
-#' nds(n = 4, s = 6)
+#' 4%d%6
 
-nds <- function(n = 1, s = 4, sum = TRUE) {
+"%d%" <- function(n, s) {
   if (!is.integer(as.integer(n))) {
     stop("Argument `n' must be coercible into an integer")
   }
@@ -19,12 +18,7 @@ nds <- function(n = 1, s = 4, sum = TRUE) {
   }
   if (n <= 0) { stop("Argument `n' must be greater than 0") }
   if (s <= 0) { stop("Argument `s' must be greater than 0") }
-  if (!is.logical(sum)) { stop("Argument `sum' must be TRUE/FALSE") }
-
-  rolls <- sample(x = 1:s, size = n, replace = TRUE)
-  if (sum) {
-    return(sum(rolls))
-  } else {
-    return(rolls)
-  }
+  rolls <- sample(1:s, n, replace = T)
+  cat(paste(c("Rolls:", rolls), collapse = " "), sep = "\n")
+  return(sum(rolls))
 }
